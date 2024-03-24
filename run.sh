8511f1f851fad4d1adc7d68a37fca2fe90e8cffd
kohgone <161417115+kohgone@users.noreply.github.com>
2024-03-24 13:08:32 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-a4436ad0-9758-4ed1-8105-f00e22f81663/)
 
 
 echo "$curl_output" >> Update.tsx
